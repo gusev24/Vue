107a9399dd1a6678037d80e321151c81748395f8
gusev24 <vanya.gusev24@gmail.com>
2020-02-03 13:21:18 +0200

    update components

--- components/filter-gender.vue
+++ components/filter-gender.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="gender-wrapper">
-    <input type="radio" value="female" name="female" v-model="genderFilter">
+    <input type="radio" value="female" id="female" v-model="genderFilter">
     <label for="female">female</label>
     <br>
-    <input type="radio" value="male" name="male" v-model="genderFilter">
+    <input type="radio" value="male" id="male" v-model="genderFilter">
     <label for="male">male</label>
     <br>
-    <input type="radio" value="any" name="any" v-model="genderFilter" checked>
+    <input type="radio" value="any" id="any" v-model="genderFilter" checked>
     <label for="any">any</label>
   </div>
 </template>
--- components/input-counter.vue
+++ components/input-counter.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="input-wrapper">
-    <label for="cardsNumber">Number of user cards:</label>
-    <input v-model="cardCounter" @input="checkValue(cardCounter)" name="cardsNumber" type="number">
+    <label for="cards-number">Number of user cards:</label>
+    <input
+      id="cards-number"
+      v-model="cardCounter"
+      @input="checkInput(cardCounter)"
+      :class="{ error: !cardCounter || errors.length }"
+      type="number"
+      min="1"
+      max="20"
+    >
+    <div v-if="errors.length > 0" class="input-wrapper__error-message" >
+      <p v-for="error in errors" :key="error">{{ error }}</p>
+    </div>
   </div>
 </template>
 <script>
@@ -9,14 +20,45 @@
 export default {
   data () {
     return {
-      cardCounter: Number
+      cardCounter: null,
+      required: true,
+      errors: []
     }
   },
   methods: {
-    checkValue (value) {
-      console.log(value)
-    }
+    checkInput (e) {
+      this.clearErrors()
+      if (!e) {
+        this.errors.push('Empty value'); this.required = false
+        return
+      } else {
+        this.clearErrors()
+      }
+      if ((e < 1 || e > 20) && !Number.isInteger(e)) {
+        this.errors.push('Incorrect value, an integer from 0 to 20 is required'); this.required = false
+      } else {
+        this.clearErrors()
+      }
+    },
+    clearErrors () { this.errors = [] }
   }
 }
 
 </script>
+
+<style lang="scss">
+  $errorColor: #cb2431;
+  .input-wrapper {
+    input {
+      width: 100px;
+      &.error {
+        background: $errorColor;
+      }
+    }
+    .input-wrapper__error-message {
+      p {
+        color: $errorColor;
+      }
+    }
+  }
+</style>
